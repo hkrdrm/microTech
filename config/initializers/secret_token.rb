ce54b8aaf625ebdf964d5a96a7bf215f79c81e73
hkrdrm <hkr.drm@gmx.com>
2013-02-14 15:21:29 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MicroTech::Application.config.secret_token = 'd002efe7ed8cddb6e6a9a9dcad7aaf41465730dbb286cc544427bd51573be194e59036df43abd9acef51018494d073f637ec8eab634ced815cb7d21b799ead9d'
